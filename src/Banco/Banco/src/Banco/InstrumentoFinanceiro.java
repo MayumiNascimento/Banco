bcdb1f7c50edf1bd90f2a40a88aefba0e5ad5126
MayumiNascimento <116170967+MayumiNascimento@users.noreply.github.com>
2022-12-13 08:32:33 -0300

    atualização das classes

---
+++ src/Banco/Banco/src/Banco/InstrumentoFinanceiro.java
@@ -0,0 +1,22 @@
+package Banco;
+
+public abstract class InstrumentoFinanceiro {
+	
+	private float saldo;
+	
+
+	public InstrumentoFinanceiro(float saldo) {
+		super();
+		this.saldo = saldo;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public abstract float CalcularSaldoTotal();
+}
